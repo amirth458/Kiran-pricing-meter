dbf3fb4dcd020e23c2730e4831a66269a5db9792
Mike O <mikiyasolika@gmail.com>
2019-08-27 18:23:10 +0300

    feat: Sidebar menu

--- frontend/src/app/common/side-menu/side-menu.component.ts
+++ frontend/src/app/common/side-menu/side-menu.component.ts
@@ -7,9 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideMenuComponent implements OnInit {
 
+  selectedRoute: string;
+  sidebarClosed = false;
+  menus = [
+    {
+      name: 'Program',
+      route: '#program',
+      icon: 'far fa-file-alt'
+    },
+    {
+      name: 'Profile',
+      route: '#profile',
+      icon: 'far fa-user-circle'
+    },
+    {
+      name: 'Fleet Control',
+      route: '#fleet',
+      icon: 'fas fa-wrench'
+    },
+    {
+      name: 'Material Inventory',
+      route: '#material',
+      icon: 'fas fa-dolly-flatbed'
+    },
+    {
+      name: 'Insight',
+      route: '#insight',
+      icon: 'far fa-chart-bar'
+    },
+    {
+      name: 'Setting',
+      route: '#setting',
+      icon: 'fas fa-cogs'
+    }
+  ];
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
+  toggleSideBar() {
+    document.querySelector('#sidebar').classList.toggle('active');
+    this.sidebarClosed = !document.querySelector('#sidebar').classList.contains('active')
+  }
+  navigateTo(route: string) {
+    this.selectedRoute = route;
+  }
 }
