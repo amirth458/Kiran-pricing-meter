922142ac2e0f83d5535405a89afb3b606225cb00
Mike O <mikiyasolika@gmail.com>
2019-09-01 17:40:08 +0300

    feat: column-search-filter

--- frontend/src/app/common/column-search-filter/column-search-filter.component.ts
+++ frontend/src/app/common/column-search-filter/column-search-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-column-search-filter',
@@ -7,9 +7,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ColumnSearchFilterComponent implements OnInit {
 
+  // Accept this from parent component
+  @Input('options') options: [string];
+  @Input('searchColumns') searchColumns: [{
+    name: string,
+    checked: boolean,
+    query: {
+      type: string,
+      queryString: string
+    }
+  }];
+  @Input('filterColumns') filterColumns: [
+    {
+      name: string,
+      checked: boolean,
+    }];
+  @Input('type') type: [string];
+
+  // Visual
+  searchColumnsClone = [];
+  filterColumnsClone = [];
+  activeTab: string;
+  showBothTab = false;
+
+  // Storage
+  searchColumnsStrorage;
+  filterColumnsStrorage;
   constructor() { }
 
   ngOnInit() {
+    this.generateSchema();
+    if (this.type.length > 0) {
+      this.activeTab = this.type[0];
+    } else {
+      this.activeTab = 'search';
+    }
+
+    if (this.type.length > 1) {
+      this.showBothTab = true;
+    }
   }
 
+  generateSchema() {
+    this.searchColumns.map((column, index) => {
+      this.searchColumnsClone.push({
+        ...column,
+        id: index
+      });
+    });
+    this.searchColumnsStrorage = this.searchColumnsClone;
+
+    this.filterColumns.map((column, index) => {
+      this.filterColumnsClone.push({
+        ...column,
+        id: index
+      });
+    });
+    this.filterColumnsStrorage = this.filterColumnsClone;
+
+  }
+
+  toggleCheck(index, type) {
+    if (type === 'search') {
+      this.searchColumns[index].checked = !this.searchColumns[index].checked;
+      this.searchColumnsClone[index].checked = !this.searchColumnsClone[index].checked;
+    } else {
+
+      this.filterColumns[index].checked = !this.filterColumns[index].checked;
+      this.filterColumnsClone[index].checked = !this.filterColumnsClone[index].checked;
+    }
+  }
+
+  toggleTab(tab) {
+    this.activeTab = tab;
+  }
+
+  search(event, type) {
+    const query = event.target.value;
+    if (type === 'search') {
+      this.searchColumnsClone = this.searchColumnsStrorage.filter(x => x.name.toString().toLowerCase().startsWith(query));
+    } else {
+      this.filterColumnsClone = this.filterColumnsStrorage.filter(x => x.name.toString().toLowerCase().startsWith(query));
+    }
+  }
 }
