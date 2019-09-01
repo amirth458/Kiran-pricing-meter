138ca96318f5011a982357302cc3abb2372a1b44
Mike O <mikiyasolika@gmail.com>
2019-09-01 18:53:23 +0300

    feat: column-search-filter accepts  custom height & width

--- frontend/src/app/common/column-search-filter/column-search-filter.component.ts
+++ frontend/src/app/common/column-search-filter/column-search-filter.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ColumnSearchFilterComponent implements OnInit {
 
   // Accept this from parent component
+  @Input('height') height: string;
+  @Input('width') width: string;
   @Input('options') options: [string];
   @Input('searchColumns') searchColumns: [{
     name: string,
