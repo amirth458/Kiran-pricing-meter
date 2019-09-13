725973c586d615421e329e886586fed5d85c09b5
Ivan Modric <lucky.man.iwan@gmail.com>
2019-09-13 10:35:50 +0200

    - fixed small issue

--- frontend/src/app/service/vendor.service.ts
+++ frontend/src/app/service/vendor.service.ts
@@ -17,24 +17,24 @@ export class VendorService {
   ) { }
 
   getVendorMetaData(type: string): Observable<VendorMetaData[]> {
-    const url = `/api/v1/vendor-metadata/${type}`;
+    const url = `${environment.apiBaseUrl}/vendor-metadata/${type}`;
     return this.http.get<any>(url).pipe(
       map(res => res.metadataList)
     );
   }
 
   getVendorDetail(id: number): Observable<Vendor> {
-    const url = `/api/v1/vendors/${id}`;
+    const url = `${environment.apiBaseUrl}/vendors/${id}`;
     return this.http.get<Vendor>(url);
   }
 
   createVendorProfile(profile: Vendor) {
-    const url = `/api/v1/vendors`;
+    const url = `${environment.apiBaseUrl}/vendors`;
     return this.http.post(url, profile);
   }
 
   updateVendorProfile(profile: Vendor) {
-    const url = `/api/v1/vendors`;
+    const url = `${environment.apiBaseUrl}/vendors`;
     return this.http.put(url, profile);
   }
 
