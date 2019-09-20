903963635fd5565dc7032769db1ff9386da3e7ef
sunil.gulabani1 <sunil.gulabani1@gmail.com>
2019-09-20 20:13:25 +0530

    Redirect /error to index.html page

---
+++ src/main/java/com/diligent/dms/webapi/MyErrorController.java
@@ -0,0 +1,22 @@
+package com.diligent.dms.webapi;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Reference: https://www.baeldung.com/spring-boot-custom-error-page
+ */
+@Controller
+public class MyErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError() {
+        return "forward:/index.html";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
