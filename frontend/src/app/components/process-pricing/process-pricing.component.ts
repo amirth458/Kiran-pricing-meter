ef797bd27bd3f2ac76edce291beef4748aafa18d
Mike O <mikiyasolika@gmail.com>
2019-09-26 02:42:07 +0300

    feat: process-pricing gridview

--- frontend/src/app/components/process-pricing/process-pricing.component.ts
+++ frontend/src/app/components/process-pricing/process-pricing.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { GridOptions } from 'ag-grid-community';
 
 import * as pricings from '../../../assets/static/processPricing';
 import { Router } from '@angular/router';
 import { ActionCellRendererComponent } from 'src/app/common/action-cell-renderer/action-cell-renderer.component';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { UserService } from 'src/app/service/user.service';
+import { ProcessPricingService } from 'src/app/service/process-pricing.service';
 
 @Component({
   selector: 'app-process-pricing',
@@ -13,6 +16,12 @@ import { ActionCellRendererComponent } from 'src/app/common/action-cell-renderer
 })
 export class ProcessPricingComponent implements OnInit {
 
+  @ViewChild('deleteModal') deleteModal;
+
+  tableControlReady = false;
+
+  processPricingParameterList: any = [];
+  processPricingConditionListprocessDimensionalPropertyList: any = [];
 
   searchColumns = [
     {
@@ -23,21 +32,21 @@ export class ProcessPricingComponent implements OnInit {
     },
     {
       name: 'Pricing Profile', checked: false,
-      field: 'pricingProfile', query: {
+      field: 'name', query: {
         type: '',
         filter: '',
       }
     },
     {
       name: 'Process Profile', checked: false,
-      field: 'processProfile', query: {
+      field: 'processProfile.name', query: {
         type: '',
         filter: '',
       }
     },
     {
       name: 'Equipment', checked: false,
-      field: 'equipment', query: {
+      field: 'processProfile.machineServingMaterial.vendorMachinery.equipment.name', query: {
         type: '',
         filter: '',
       }
@@ -45,104 +54,27 @@ export class ProcessPricingComponent implements OnInit {
 
     {
       name: 'Material', checked: false,
-      field: 'material', query: {
+      field: 'processProfile.machineServingMaterial.material.name', query: {
         type: '',
         filter: '',
       }
-    },
-    {
-      name: 'Condition 1', checked: false,
-      field: 'condition1', query: {
-        type: '',
-        filter: '',
-      }
-    },
-    {
-      name: 'Condition 2', checked: false,
-      field: 'condition2', query: {
-        type: '',
-        filter: '',
-      }
-    },
-    {
-      name: 'Condition 3', checked: false,
-      field: 'condition3', query: {
-        type: '',
-        filter: '',
-      }
-    },
-    {
-      name: 'Pricing Component 1', checked: false,
-      field: 'pricingComponent1', query: {
-        type: '',
-        filter: '',
-      }
-    },
-    {
-      name: 'Pricing Component 2', checked: false,
-      field: 'pricingComponent2', query: {
-        type: '',
-        filter: '',
-      }
-    },
-    {
-      name: 'Pricing Component 3', checked: false,
-      field: 'pricingComponent3', query: {
-        type: '',
-        filter: '',
-      }
-    },
-    {
-      name: 'Pricing Component 4', checked: false,
-      field: 'pricingComponent4', query: {
-        type: '',
-        filter: '',
-      }
-    },
-    {
-      name: 'Actions', checked: false,
-      field: 'actions', query: {
-        type: '',
-        filter: '',
-      }
-    },
+    }
   ];
   filterColumns = [
     {
       name: 'Pricing No', checked: true, field: 'id'
     },
     {
-      name: 'Pricing Profile', checked: true, field: 'pricingProfile'
+      name: 'Pricing Profile', checked: true, field: 'name'
     },
     {
-      name: 'Process Profile', checked: true, field: 'processProfile'
+      name: 'Process Profile', checked: true, field: 'processProfile.name'
     },
     {
-      name: 'Equipment', checked: true, field: 'equipment'
+      name: 'Equipment', checked: true, field: 'processProfile.machineServingMaterial.vendorMachinery.equipment.name'
     },
     {
-      name: 'Material', checked: true, field: 'material'
-    },
-    {
-      name: 'Condition 1', checked: true, field: 'condition1'
-    },
-    {
-      name: 'Condition 2', checked: false, field: 'condition2'
-    },
-    {
-      name: 'Condition 3', checked: false, field: 'condition3'
-    },
-    {
-      name: 'Pricing Component 1', checked: true, field: 'pricingComponent1'
-    },
-    {
-      name: 'Pricing Component 2', checked: false, field: 'pricingComponent2'
-    },
-    {
-      name: 'Pricing Component 3', checked: false, field: 'pricingComponent3'
-    },
-    {
-      name: 'Pricing Component 4', checked: false, field: 'pricingComponent4'
+      name: 'Material', checked: true, field: 'processProfile.machineServingMaterial.material.name'
     }
   ];
   type = ['search', 'filter'];
@@ -151,44 +83,33 @@ export class ProcessPricingComponent implements OnInit {
     actionCellRenderer: ActionCellRendererComponent,
   };
 
-  columnDefs = [
+  columnDefs: Array<any> = [
     { headerName: 'Pricing No', field: 'id', hide: false, sortable: true, filter: true, },
-    { headerName: 'Pricing Profile', field: 'pricingProfile', hide: false, sortable: true, filter: true },
-    { headerName: 'Process Profile', field: 'processProfile', hide: false, sortable: true, filter: true },
-    { headerName: 'Equipment', field: 'equipment', hide: false, sortable: true, filter: true },
-    { headerName: 'Material', field: 'material', hide: false, sortable: true, filter: true },
-    { headerName: 'Condition 1', field: 'condition1', hide: false, sortable: true, filter: true },
-    { headerName: 'Condition 2', field: 'condition2', hide: false, sortable: true, filter: true },
-    { headerName: 'Condition 3', field: 'condition3', hide: false, sortable: true, filter: true },
-    { headerName: 'Pricing Component 1', field: 'pricingComponent1', hide: false, sortable: true, filter: true },
-    { headerName: 'Pricing Component 2', field: 'pricingComponent2', hide: false, sortable: true, filter: true },
-    { headerName: 'Pricing Component 3', field: 'conpricingComponent3dition3', hide: false, sortable: true, filter: true },
-    { headerName: 'Pricing Component 4', field: 'pricingComponent14', hide: false, sortable: true, filter: true },
-    {
-      headerName: 'Actions',
-      width: 100,
-      cellRenderer: 'actionCellRenderer',
-      cellRendererParams: {
-        action: {
-          edit: (param) => this.editRow(param),
-          delete: (param) => this.deleteRow(param),
-          canEdit: true,
-          canCopy: false,
-          canDelete: true,
-        }
-      }
-    }
+    { headerName: 'Pricing Profile', field: 'name', hide: false, sortable: true, filter: true },
+    { headerName: 'Process Profile', field: 'processProfile.name', hide: false, sortable: true, filter: true },
+    // tslint:disable-next-line:max-line-length
+    { headerName: 'Equipment', field: 'processProfile.machineServingMaterial.vendorMachinery.equipment.name', hide: false, sortable: true, filter: true },
+    { headerName: 'Material', field: 'processProfile.machineServingMaterial.material.name', hide: false, sortable: true, filter: true },
   ];
-
 
   gridOptions: GridOptions;
 
   rowData;
   pageSize = 10;
-  constructor(public route: Router) { }
+  constructor(
+    public route: Router,
+    public spineer: NgxSpinnerService,
+    public userService: UserService,
+    public processPricingService: ProcessPricingService) { }
 
-  ngOnInit() {
-    this.rowData = pricings;
+  async ngOnInit() {
+    this.spineer.show();
+
+    await this.getProfiles();
+    this.createColumns();
+
+    this.tableControlReady = true;
+
     if (this.type.includes('filter')) {
       this.configureColumnDefs();
     }
@@ -259,5 +180,147 @@ export class ProcessPricingComponent implements OnInit {
     this.gridOptions.api.setColumnDefs([]);
     this.gridOptions.api.setColumnDefs(this.columnDefs);
     this.gridOptions.api.sizeColumnsToFit();
+  }
+
+  async getProfiles() {
+    try {
+      const res = await this.processPricingService.getAllProfiles(this.userService.getVendorInfo().id).toPromise();
+      this.rowData = res || [];
+    } catch (e) {
+      console.log(e);
+    } finally {
+      this.spineer.hide();
+    }
+  }
+
+  createColumns() {
+    const conditionNames = [];
+    const componentName = [];
+
+    this.rowData.map(row => {
+      row.processPricingConditionList.map((x, index) => {
+        if (!conditionNames.includes(`Condition ${index + 1}`)) {
+          conditionNames.push(`Condition ${index + 1}`);
+          // this.searchColumns.push(
+          //   {
+          //     name: `Condition ${index + 1}`, checked: false, field: `condition${index + 1}`, query: {
+          //       type: '',
+          //       filter: '',
+          //     }
+          //   });
+          this.filterColumns.push(
+            {
+              name: `Condition ${index + 1}`, checked: false, field: `condition${index + 1}`
+            });
+
+          this.columnDefs.push({
+            headerName: `Condition ${index + 1}`,
+            field: `condition${index + 1}`,
+            hide: false,
+            sortable: true,
+            filter: true,
+            cellRenderer(params: any): any {
+              let value = '';
+              params.data.processPricingConditionList.map(item => {
+                if (item.id === x.id) {
+                  value = `${item.processPricingConditionType.name} ${item.operatorType.symbol} `;
+                  if (item.valueSignType == 'positive') {
+                    value += `${item.value} ${item.unitType.symbol}`;
+                  } else if (item.valueSignType === 'absolute') {
+                    value += `|${item.value}| ${item.unitType.symbol}`;
+                  } else {
+                    value += `-${item.value} ${item.unitType.symbol}`;
+                  }
+                }
+              });
+              return value;
+            }
+          });
+        }
+      });
+
+      row.processPricingParameterList.map((x, index) => {
+        if (!componentName.includes(`Pricing Component ${index + 1}`)) {
+          componentName.push(`Pricing Component ${index + 1}`);
+          // this.searchColumns.push(
+          //   {
+          //     name: `Pricing Component ${index + 1}`, checked: false, field: `pricingComponent${index + 1}`, query: {
+          //       type: '',
+          //       filter: '',
+          //     }
+          //   });
+          this.filterColumns.push(
+            {
+              name: `Pricing Component ${index + 1}`, checked: false, field: `pricingComponent${index + 1}`
+            });
+
+          this.columnDefs.push({
+            headerName: `Pricing Component ${index + 1}`,
+            field: `pricingComponent${index + 1}`,
+            hide: true,
+            sortable: true,
+            filter: true,
+            cellRenderer(params: any): any {
+              let value = '';
+              params.data.processPricingParameterList.map(item => {
+                if (item.id == x.id) {
+                  value = `${item.currency.symbol}${item.price} / ${item.quantityUnitType.name}`;
+                }
+              });
+              return value;
+            }
+          });
+        }
+      });
+    });
+
+
+    this.columnDefs.push({
+      headerName: 'Actions',
+      width: 140,
+      cellRenderer: 'actionCellRenderer',
+      cellRendererParams: {
+        action: {
+          edit: (param) => this.editRow(param),
+          copy: (param) => {
+          },
+          delete: async (param) => {
+            this.deleteModal.nativeElement.click();
+          },
+          canEdit: true,
+          canCopy: false,
+          canDelete: true,
+        }
+      }
+    });
+
+    let conditionActive = false;
+    let componentActive = false;
+    this.filterColumns = this.filterColumns.map((column) => {
+      if (!column.checked && (!componentActive || !conditionActive)) {
+        if (column.name.startsWith('Condition') && !conditionActive) {
+          conditionActive = true;
+          return { ...column, checked: true };
+        }
+        if (column.name.startsWith('Pricing') && !componentActive) {
+          componentActive = true;
+          return { ...column, checked: true };
+        }
+        return { ...column };
+      } else {
+        return { ...column };
+      }
+    });
+
+  }
+
+  getRandomString(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
   }
 }
