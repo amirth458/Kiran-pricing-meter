c46c3b24288a8488a9a5fdfe6b302ae9debb81aa
Mike O <mikiyasolika@gmail.com>
2019-10-08 18:16:21 +0300

    feat: added httpInterceptor

---
+++ frontend/src/app/http-interceptors/auth-inteceptor.ts
@@ -0,0 +1,24 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+
+
+
+@Injectable()
+
+export class AuthInterceptor implements HttpInterceptor {
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const localAuthInfo = localStorage.getItem('auth'); // you probably want to store it in localStorage or something
+
+        if (!localAuthInfo) {
+            return next.handle(req);
+        }
+
+        const token = JSON.parse(localAuthInfo);
+        const req1 = req.clone({
+            headers: req.headers.set('Authorization', `${token.tokenType} ${token.accessToken}`),
+        });
+
+        return next.handle(req1);
+    }
+}
