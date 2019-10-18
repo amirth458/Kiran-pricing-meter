a94362c75c30950a93f89403dec19f688a2b5f11
sunil.gulabani1 <sunil.gulabani1@gmail.com>
2019-10-18 08:21:49 +0530

    Removed duplicate Web Config

--- src/main/java/com/diligent/dms/config/WebMvcConfig.java
+++ src/main/java/com/diligent/dms/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.diligent.dms.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,5 +16,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
                 .maxAge(MAX_AGE_SECS);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // forward requests to /admin and /user to their index.html
+        registry.addViewController("/").setViewName(
+                "forward:/index.html");
     }
 }
