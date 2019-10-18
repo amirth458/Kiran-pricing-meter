ef0a8f02ba9f72bb5a250d7709acbb6633dce0e6
Mike O <mikiyasolika@gmail.com>
2019-10-18 16:39:39 +0300

    feat: estimatePrice service

--- frontend/src/app/service/profile-screener.service.ts
+++ frontend/src/app/service/profile-screener.service.ts
@@ -26,7 +26,29 @@ export class ProfileScreenerService {
       surfaceRoughness: 1,
       surfaceFinish: 1
     };
-    return this.http.post(environment.PPE_HOST_URL + '/profile', { ...fakeBody, userId: vendorId });
+    return this.http.post(environment.PPE_HOST_URL + '/profile', { ...body, userId: vendorId });
+    // return this.http.post(environment.PPE_HOST_URL + '/profile', { ...fakeBody });
+  }
+
+  estimatePrice(vendorId: number, body): Observable<any> {
+    const fakeBody = {
+      userId: 1,
+      profileTypeId: 1,
+      customerIndustryId: 1,
+      requiredCertificateId: 1,
+      materialId: 1,
+      equipmentId: 1,
+      deliveryStatementId: 1,
+      nda: false,
+      tolerance: 1,
+      surfaceRoughness: 1,
+      surfaceFinish: 1,
+      processProfileIdList: [
+        1, 2
+      ]
+    };
+    return this.http.post(environment.PPE_HOST_URL + '/profile', { ...body, userId: vendorId });
+    // return this.http.post(environment.PPE_HOST_URL + '/pre-quote', { ...fakeBody });
   }
 
 }
