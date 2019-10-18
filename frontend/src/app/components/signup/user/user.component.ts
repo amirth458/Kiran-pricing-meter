4341c12abd5fd9a430a55ecee9ef42c8e7f122cb
Ivan Modric <lucky.man.iwan@gmail.com>
2019-10-18 09:10:46 -0700

    update registration page

--- frontend/src/app/components/signup/user/user.component.ts
+++ frontend/src/app/components/signup/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewChecked } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, HostListener, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from '../../../service/user.service';
@@ -17,14 +17,26 @@ export class RegisterUserComponent implements OnInit, AfterViewChecked {
     passwordConfirm: [null, Validators.required],
     phone: [null, Validators.required],
   });
+  @ViewChild('infoModal') infoModal;
   isLoadedUser = false;
   errorMessage = '';
+  isValid = false;
   constructor(
     private fb: FormBuilder,
     private router: Router,
     private userService: UserService
   ) { }
-
+  @HostListener('window:beforeunload', ['$event'])
+  unloadNotification($event: any) {
+    if (this.form.valid) {
+      const user: RegisterUser = {
+        ...this.form.value,
+      };
+      this.userService.setRegisterUserInfo(user);
+      return true;
+    }
+    return false;
+  }
   ngOnInit() {
     const user: RegisterUser = this.userService.getRegisterUserInfo();
     if (user) {
@@ -34,7 +46,16 @@ export class RegisterUserComponent implements OnInit, AfterViewChecked {
   }
   ngAfterViewChecked(): void {
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.getElementsByClassName('needs-validation');
+    if (this.form.valid) {
+      const user: RegisterUser = {
+        ...this.form.value,
+      };
+      this.userService.setRegisterUserInfo(user);
+    }
+    if (this.isValid !== this.form.valid) {
+      this.isValid = this.form.valid;
+      this.userService.setUserFormStatus(this.form.valid ? 1 : 0);
+    }
   }
 
   initUser(user: RegisterUser): void {
@@ -67,14 +88,9 @@ export class RegisterUserComponent implements OnInit, AfterViewChecked {
 
     this.errorMessage = '';
     if (!(this.form.valid)) {
+      this.infoModal.nativeElement.click();
       return;
     }
-    const user: RegisterUser = {
-      ...this.form.value,
-      company: '',
-      department: '',
-    };
-    this.userService.setRegisterUserInfo(user);
     this.router.navigateByUrl('/signup/vendor');
   }
 }
