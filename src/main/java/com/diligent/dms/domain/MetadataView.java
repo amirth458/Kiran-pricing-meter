acd8b2f5aad4a11b46e53451b7518eb60019f86d
sunil.gulabani1 <sunil.gulabani1@gmail.com>
2019-10-20 15:38:55 +0530

    - Upload File to Connector Service - Get Metadata from Connector Service

---
+++ src/main/java/com/diligent/dms/domain/MetadataView.java
@@ -0,0 +1,18 @@
+package com.diligent.dms.domain;
+
+import lombok.*;
+
+import java.util.Map;
+
+@Getter
+@Setter
+@Builder(builderMethodName = "builder")
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class MetadataView {
+    private Long id;
+    private String fileName;
+    private String s3Url;
+    private String status;
+    private Map<String, Object> forgeAPIMetadata;
+}
--- src/main/java/com/diligent/dms/service/ConnectorService.java
+++ src/main/java/com/diligent/dms/service/ConnectorService.java
@@ -1,15 +1,13 @@
 package com.diligent.dms.service;
 
-import com.diligent.dms.domain.ConnectorLoginRequest;
-import com.diligent.dms.domain.ConnectorLoginResponse;
+import com.diligent.dms.domain.*;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import jdk.nashorn.internal.objects.annotations.Getter;
-import jdk.nashorn.internal.objects.annotations.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
@@ -17,17 +15,21 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.File;
-import java.util.Map;
 
+import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.http.MediaType.MULTIPART_FORM_DATA;
 
 @Component
 public class ConnectorService {
 
-    private static final String CONNECTOR_API_PREFIX = "http://connector-service-2.us-west-2.elasticbeanstalk.com";
+    private String connectorServiceHostURL;
 
-    public void uploadFile(File file) {
+    public ConnectorService(@Value("${connector.service.host.url}") String connectorServiceHostURL) {
+        this.connectorServiceHostURL = connectorServiceHostURL;
+    }
+
+    public ConnectorUploadFileResponse uploadFile(File file) {
         MultiValueMap<String, Object> bodyMap = new LinkedMultiValueMap<>();
         bodyMap.add("file", new FileSystemResource(file));
         HttpHeaders headers = new HttpHeaders();
@@ -36,15 +38,16 @@ public class ConnectorService {
         HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(bodyMap, headers);
 
         RestTemplate restTemplate = new RestTemplate();
-        String uri = CONNECTOR_API_PREFIX + "/api/v1/file";
-        ResponseEntity<String> response = restTemplate.exchange(uri, POST, requestEntity, String.class);
-
+        String uri = connectorServiceHostURL + "/api/v1/file";
+        ResponseEntity<ConnectorUploadFileResponse> responseEntity = restTemplate.exchange(uri, POST, requestEntity, ConnectorUploadFileResponse.class);
+        return responseEntity.getBody();
     }
 
     private String getToken() {
         RestTemplate restTemplate = new RestTemplate();
-        String uri = CONNECTOR_API_PREFIX + "/api/v1/users/signin";
+        String uri = connectorServiceHostURL + "/api/v1/users/signin";
         HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
 
         ConnectorLoginRequest connectorLoginRequest = new ConnectorLoginRequest();
         connectorLoginRequest.setEmail("shop_manage_admin@3diligent.com");
@@ -55,5 +58,17 @@ public class ConnectorService {
         ResponseEntity<ConnectorLoginResponse> response = restTemplate.exchange(uri, POST, requestEntity, ConnectorLoginResponse.class);
         ConnectorLoginResponse connectorLoginResponse = response.getBody();
         return connectorLoginResponse.getAccessToken();
+    }
+
+    public ConnectorGetMetadataResponse getMetadata(Long metadataId) {
+        RestTemplate restTemplate = new RestTemplate();
+        String uri = connectorServiceHostURL + "/api/v1/metadata/" + metadataId;
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(getToken());
+
+        HttpEntity<String> requestEntity = new HttpEntity<>(headers);
+        ResponseEntity<MetadataView> response = restTemplate.exchange(uri, GET, requestEntity, MetadataView.class);
+        ConnectorGetMetadataResponse connectorGetMetadataResponse = ConnectorGetMetadataResponse.map(response.getBody());
+        return connectorGetMetadataResponse;
     }
 }
