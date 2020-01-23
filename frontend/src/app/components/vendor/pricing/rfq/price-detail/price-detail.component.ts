53ecba4f70b38c9c8a226c771f5e0164bf422528
Sarvaratchagan <sarvaratchagan@outlook.com>
2020-01-23 10:02:52 +0530

    refactor: improve tab title and other info

--- frontend/src/app/components/vendor/pricing/rfq/price-detail/price-detail.component.ts
+++ frontend/src/app/components/vendor/pricing/rfq/price-detail/price-detail.component.ts
@@ -17,24 +17,22 @@ import { NgxSpinnerService } from "ngx-spinner";
   templateUrl: "./price-detail.component.html",
   styleUrls: ["./price-detail.component.css"]
 })
-export class PriceDetailComponent implements OnInit {
-  selectedId: number;
-  part: Part;
-  rfq: RfqData;
-  partQuote: PartQuote;
-  partDimension: PartDimension;
-  customer: CustomerData;
-
-  tabs = [];
-
-  selectedTabId$: BehaviorSubject<number> = new BehaviorSubject(0);
+export class PriceDetailComponent implements OnInit  {
+  protected selectedId: number;
+  protected part: Part;
+  protected rfq: RfqData;
+  protected partQuote: PartQuote;
+  protected partDimension: PartDimension;
+  protected customer: CustomerData;
+  protected tabs = [];
+  protected selectedTabId$: BehaviorSubject<number> = new BehaviorSubject(0);
 
   constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-    private pricingService: RfqPricingService,
-    private userService: UserService,
-    private spinner: NgxSpinnerService
+    protected  route: ActivatedRoute,
+    protected  router: Router,
+    protected  pricingService: RfqPricingService,
+    protected  userService: UserService,
+    protected  spinner: NgxSpinnerService
   ) {
     this.route.params.subscribe(params => {
       this.selectedId = params.partId;
@@ -42,30 +40,11 @@ export class PriceDetailComponent implements OnInit {
     });
   }
 
-  getDetails(id: number) {
+  protected getDetails(id: number) {
     this.spinner.show();
     this.pricingService.getPartDetail(id).subscribe(part => {
       this.spinner.hide();
       this.part = part;
-      this.tabs = [
-        {
-          id: 0,
-          title:
-            this.part && this.part.manualPricingAllowed
-              ? "Manual-Price View"
-              : "Auto-Price View"
-        },
-        {
-          id: 1,
-          title: "Part Information"
-        },
-        {
-          id: 2,
-          title: part.manualPricingAllowed
-            ? "Process Profile"
-            : "Pricing Profile"
-        }
-      ];
       this.pricingService
         .getRfqDetail(this.part.rfqMedia.projectRfqId)
         .subscribe(rfq => {
@@ -84,10 +63,33 @@ export class PriceDetailComponent implements OnInit {
         .subscribe(dimension => {
           this.partDimension = dimension;
         });
+      this.setTabInfo();
     });
   }
 
-  manualQuote() {
+  protected setTabInfo() {
+    this.tabs = [
+      {
+        id: 0,
+        title:
+          this.part && this.part.manualPricingAllowed
+            ? "Manual-Price View"
+            : "Auto-Price View"
+      },
+      {
+        id: 1,
+        title: "Part Information"
+      },
+      {
+        id: 2,
+        title: this.part.manualPricingAllowed
+          ? "Process Profile"
+          : "Pricing Profile"
+      }
+    ];
+  }
+
+  protected manualQuote() {
     this.getDetails(this.selectedId);
   }
 
