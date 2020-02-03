3987846188664fb78770f6730a17adbc2b04b1f8
bitneuron <messagekr@gmail.com>
2020-02-02 21:26:15 -0800

    updated dev environment.ts

--- frontend/src/environments/environment.dev.ts
+++ frontend/src/environments/environment.dev.ts
@@ -48,7 +48,7 @@ export const environment = {
   ],
   apiBaseUrl: "https://dev-admin-api.3diligent.com/api/v1",
   marketApiBaseUrl: "http://dev-marketplace-api.3diligent.com/api/v1",
-  managementBaseUrl: "http://dev-manage-api.3diligent.com/api/v1",
+  managementBaseUrl: "https://dev-manage-api.3diligent.com/api/v1",
   procurementApiBaseUrl: "http://dev-procurement-api.3diligent.com/api/v1",
   adminAPIToken: "655d67be-af74-48bb-b707-ce5266f7e8fc",
   MARKETPLACE_URL: "http://dev-marketplace.3diligent.com/",
