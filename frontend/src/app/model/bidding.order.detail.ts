9e36bb3bbfd888d9749b38484a21be2a91883f2f
sarvaratchagan <sarvaratchagan@outlook.com>
2020-02-26 17:34:03 +0100

    refactor: schema updates

--- frontend/src/app/model/bidding.order.detail.ts
+++ frontend/src/app/model/bidding.order.detail.ts
@@ -16,24 +16,43 @@ export interface VendorOrderDetail {
   bidProcessId: number;
 }
 
-interface GetAllCustomerPartView {
-  subOrder: number;
+export interface GetAllCustomerPartView {
+  partId: number;
+  bidOrderItemId: number;
   customerOrder: number;
   priceAccepted: number;
+  materialPropertyType: string;
+  materialPropertyValues: Array<string>;
+  equipmentPropertyType: string;
+  equipmentPropertyValues: Array<string>;
   customerName: string;
   quantity: number;
-  materialName: string;
-  process: string;
   postProcessTypeNames: string;
   shippedDate: Date;
   deliveryDate: Date;
-  partStatusType: any;
+  partStatusType: string;
   fileName: string;
+  connectorServiceId: number;
+  filePath: string;
   nda: string;
-  materialId: number;
-  processTypeId: number;
   postProcessTypeIds: Array<number>;
   partStatusTypeId: number;
   rfqMediaId: number;
-  totalCount: number;
+  surfaceRoughness: string;
+  tolerance: Array<string>;
+  x: ValueUnit;
+  y: ValueUnit;
+  z: ValueUnit;
+  volume: ValueUnit;
+  surfaceArea: ValueUnit;
+  thumbnail100Location: string;
+  thumbnail200Location: string;
+  thumbnail400Location: string;
+  shippingAddress: string;
+}
+
+export interface ValueUnit {
+  value: number;
+  valueInDefaultUnit: number;
+  unitId: number;
 }
