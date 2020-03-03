d1bacca0373527c94a15447f99e920da3f6faed0
bearandyoon <lucky.man.iwan@gmail.com>
2020-03-03 17:12:30 -0500

    fix: pricing setting

--- frontend/src/app/components/vendor/pricing/rfq/pricing-settings/pricing-settings.component.ts
+++ frontend/src/app/components/vendor/pricing/rfq/pricing-settings/pricing-settings.component.ts
@@ -3,7 +3,12 @@ import { ToastrService } from "ngx-toastr";
 import { HttpErrorResponse } from "@angular/common/http";
 import { catchError } from "rxjs/operators";
 import { RfqPricingService } from "./../../../../../service/rfq-pricing.service";
-import { FormBuilder, FormGroup } from "@angular/forms";
+import {
+  FormBuilder,
+  FormGroup,
+  ValidatorFn,
+  AbstractControl
+} from "@angular/forms";
 import { Component, OnInit } from "@angular/core";
 import { throwError } from "rxjs";
 
@@ -13,16 +18,19 @@ import { throwError } from "rxjs";
   styleUrls: ["./pricing-settings.component.css"]
 })
 export class PricingSettingsComponent implements OnInit {
-  detailForm: FormGroup = this.fb.group({
-    minElligibleProcessProfile: [null],
-    minElligiblePricingProfile: [null],
-    presentBidNumberFromBottom: [null],
-    incrementalMarginPercent: [null],
-    incrementalMarginRate: [null],
-    specificityPremium: [null],
-    autoPricingEligibilityType: [null],
-    quoteExpirationTime: [null]
-  });
+  detailForm: FormGroup = this.fb.group(
+    {
+      minElligibleProcessProfile: [null],
+      minElligiblePricingProfile: [null],
+      presentBidNumberFromBottom: [null],
+      incrementalMarginPercent: [null],
+      incrementalMarginRate: [null],
+      specificityPremium: [null],
+      autoPricingEligibilityType: [null],
+      quoteExpirationTime: [null]
+    },
+    { validators: this.checkBidNumber }
+  );
   manualPricingSection = 3;
 
   defaultEligibilities = [
@@ -47,6 +55,17 @@ export class PricingSettingsComponent implements OnInit {
     private actionService: ActionService
   ) {}
 
+  checkBidNumber(control: FormGroup) {
+    const bidNumber = control.get("presentBidNumberFromBottom");
+    const processProfile = control.get("minElligibleProcessProfile");
+
+    return bidNumber &&
+      processProfile &&
+      (+bidNumber.value || 0) > (+processProfile.value || 0)
+      ? { invalidBidNumber: true }
+      : null;
+  }
+
   ngOnInit() {
     this.pricingService.getPricingSettings().subscribe(defaultValue => {
       if (defaultValue) {
@@ -61,13 +80,16 @@ export class PricingSettingsComponent implements OnInit {
   }
 
   async save() {
-    this.pricingService
-      .setPricingSetting(this.detailForm.value)
-      .pipe(catchError(e => this.handleSaveError(e)))
-      .subscribe(v => {
-        this.detailForm.setValue(v);
-        this.toastrService.success(`Pricing Settings Updated Successfully`);
-      });
+    console.log(this.detailForm);
+    if (this.detailForm.valid) {
+      this.pricingService
+        .setPricingSetting(this.detailForm.value)
+        .pipe(catchError(e => this.handleSaveError(e)))
+        .subscribe(v => {
+          this.detailForm.setValue(v);
+          this.toastrService.success(`Pricing Settings Updated Successfully`);
+        });
+    }
   }
 
   handleSaveError(error: HttpErrorResponse) {
