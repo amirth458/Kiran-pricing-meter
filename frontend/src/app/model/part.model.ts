ab817e466355e17e31e0f560b916d91d289ac17b
bearandyoon <lucky.man.iwan@gmail.com>
2020-04-03 10:55:29 -0400

    fix: show vendor name in pricing profile details

--- frontend/src/app/model/part.model.ts
+++ frontend/src/app/model/part.model.ts
@@ -303,6 +303,7 @@ export interface ProcessProfile {
   name: string;
   parameterNickName: string;
   vendorId: number;
+  vendorName?: string;
   vendorEmailAddress?: string;
   processProfileType: Type;
   processAction: any;
