e3a6c8d91782b1dae45eb3f1516c668a4284e17b
Mike O <mikiyasolika@gmail.com>
2020-05-29 14:45:26 +0300

    feat: dummy grid data and model

--- frontend/src/app/service/project.service.ts
+++ frontend/src/app/service/project.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { FilterOption } from '../model/vendor.model';
+import { of } from 'rxjs';
+import { StatusTypes } from '../model/connect.model';
 
 @Injectable({
   providedIn: 'root'
@@ -51,5 +53,128 @@ export class ProjectService {
       params = params.append('sort', filterOption.sort.toString());
     }
     return this.http.post<any>(url, { statusIds: [3], projectType }, { params });
+  }
+
+  getProdExSupplier() {
+    return of([
+      {
+        vendor: 'Vendor',
+        vendorName: 'Vendor Name',
+        city: 'City',
+        state: 'State',
+        status: {
+          id: StatusTypes.ACCEPTED
+        }
+      },
+      {
+        vendor: 'Vendor',
+        vendorName: 'Vendor Name',
+        city: 'City',
+        state: 'State',
+        status: {
+          id: StatusTypes.DECLINED
+        }
+      },
+      {
+        vendor: 'Vendor',
+        vendorName: 'Vendor Name',
+        city: 'City',
+        state: 'State',
+        status: {
+          id: StatusTypes.ACCEPTED
+        }
+      },
+      {
+        vendor: 'Vendor',
+        vendorName: 'Vendor Name',
+        city: 'City',
+        state: 'State',
+        status: {
+          id: StatusTypes.AWAITING_FOR_RESPONSE
+        }
+      }
+    ]);
+  }
+
+  getCustomerSelectedSupplier() {
+    return of([
+      {
+        vendor: 'Vendor',
+        vendorName: 'Vendor Name',
+        city: 'City',
+        state: 'State',
+        status: {
+          id: StatusTypes.ACCEPTED
+        }
+      },
+      {
+        vendor: 'Vendor',
+        vendorName: 'Vendor Name',
+        city: 'City',
+        state: 'State',
+        status: {
+          id: StatusTypes.ACCEPTED
+        }
+      },
+      {
+        vendor: 'Vendor',
+        vendorName: 'Vendor Name',
+        city: 'City',
+        state: 'State',
+        status: {
+          id: StatusTypes.ACCEPTED
+        }
+      },
+      {
+        vendor: 'Vendor',
+        vendorName: 'Vendor Name',
+        city: 'City',
+        state: 'State',
+        status: {
+          id: StatusTypes.DECLINED
+        }
+      }
+    ]);
+  }
+
+  getCustomerSupplierToInvite() {
+    return of([
+      {
+        vendorName: 'Vendor Name',
+        contactName: 'Contact Name',
+        email: 'Email',
+        phone: 'Phone',
+        status: {
+          id: StatusTypes.ACCEPTED
+        }
+      },
+      {
+        vendorName: 'Vendor Name',
+        contactName: 'Contact Name',
+        email: 'Email',
+        phone: 'Phone',
+        status: {
+          id: StatusTypes.DECLINED
+        }
+      },
+      {
+        vendorName: 'Vendor Name',
+        contactName: 'Contact Name',
+        email: 'Email',
+        phone: 'Phone',
+        status: {
+          id: StatusTypes.AWAITING_FOR_RESPONSE
+        }
+      },
+      {
+        vendorName: 'Vendor Name',
+        contactName: 'Contact Name',
+        email: 'Email',
+        phone: 'Phone',
+        status: {
+          id: StatusTypes.ACCEPTED
+        }
+      }
+    ]);
   }
 }
