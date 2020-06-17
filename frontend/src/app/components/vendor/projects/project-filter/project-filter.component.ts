916c38eb4b1b7dc2cfb73ca05f87926a3def7aa7
sarvaratchagan <sarvaratchagan@outlook.com>
2020-06-18 01:09:44 +0200

    feat - project filter component

---
+++ frontend/src/app/components/vendor/projects/project-filter/project-filter.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-project-filter',
+  templateUrl: './project-filter.component.html',
+  styleUrls: ['./project-filter.component.css']
+})
+export class ProjectFilterComponent implements OnInit {
+  value: FormGroup = this.fb.group({
+    projectTypeId: [null],
+    startDate: [null],
+    endDate: [null],
+    searchQuery: null
+  });
+
+  constructor(public fb: FormBuilder) {}
+
+  ngOnInit() {}
+}
