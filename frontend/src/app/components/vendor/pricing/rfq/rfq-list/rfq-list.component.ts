b0fb14e3274b39a4ff90c3f407c4a0734e73fe4b
sarvaratchagan <sarvaratchagan@outlook.com>
2020-07-14 13:03:34 +0200

    feat - grid for auto rfq

--- frontend/src/app/components/vendor/pricing/rfq/rfq-list/rfq-list.component.ts
+++ frontend/src/app/components/vendor/pricing/rfq/rfq-list/rfq-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { NgxSpinnerService } from 'ngx-spinner';
+
+import { ColDef, GridOptions } from 'ag-grid-community';
 
 @Component({
   selector: 'app-rfq-list',
@@ -6,7 +11,114 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rfq-list.component.css']
 })
 export class RfqListComponent implements OnInit {
-  constructor() {}
+  columnDefs: ColDef[] = [];
+  gridOptions: GridOptions;
+  pageSize = 10;
 
-  ngOnInit() {}
+  constructor(public spinner: NgxSpinnerService, public router: Router) {}
+
+  ngOnInit() {
+    this.initColumns();
+    this.gridOptions = {
+      columnDefs: this.columnDefs,
+      paginationPageSize: this.pageSize,
+      maxConcurrentDatasourceRequests: 1,
+      rowModelType: 'infinite',
+      enableColResize: true,
+      rowHeight: 35,
+      headerHeight: 35,
+      rowBuffer: 0,
+      cacheBlockSize: this.pageSize,
+      infiniteInitialRowCount: 0,
+      cacheOverflowSize: 0
+    };
+  }
+
+  initColumns() {
+    this.columnDefs = [
+      {
+        headerName: 'RFQ ID',
+        field: 'rfqId',
+        hide: false,
+        sortable: true,
+        filter: false,
+        tooltipField: 'rfqId'
+      },
+      {
+        headerName: 'RFQ Profile',
+        field: 'rfqProfile',
+        hide: false,
+        sortable: true,
+        filter: false,
+        tooltipField: 'rfqProfile'
+      },
+      {
+        headerName: 'Status',
+        field: 'rfqStatus',
+        hide: false,
+        sortable: true,
+        filter: false,
+        tooltipField: 'rfqStatus',
+        valueFormatter: v => (v.value || '').replace(/_/g, ' ')
+      },
+      {
+        headerName: 'Customer Name',
+        field: 'customerName',
+        hide: false,
+        sortable: true,
+        filter: false,
+        tooltipField: 'customerName'
+      },
+      {
+        headerName: 'Customer Contact Name',
+        field: 'userName',
+        hide: false,
+        sortable: true,
+        filter: false,
+        tooltipField: 'userName'
+      },
+      {
+        headerName: 'RFQ Created',
+        field: 'rfqCreatedAt',
+        hide: false,
+        sortable: true,
+        filter: false,
+        tooltipField: 'rfqCreatedAt'
+      },
+      {
+        headerName: 'Eligible Manufacturer Type',
+        field: 'eligible_manufacturer_types',
+        hide: false,
+        sortable: true,
+        filter: false,
+        tooltipField: 'eligible_manufacturer_types'
+      },
+      {
+        headerName: 'Eligible Regions',
+        field: 'eligible_regions',
+        hide: false,
+        sortable: true,
+        filter: false,
+        tooltipField: 'eligible_regions'
+      },
+      {
+        headerName: 'Facility Quality Certifications',
+        field: 'facility_certificates',
+        hide: false,
+        sortable: true,
+        filter: false,
+        tooltipField: 'facility_certificates'
+      },
+      {
+        headerName: 'Part Certifications',
+        field: 'part_certificates',
+        hide: false,
+        sortable: true,
+        filter: false,
+        tooltipField: 'part_certificates'
+      }
+    ];
+  }
+
+  onGridReady(event) {}
 }
