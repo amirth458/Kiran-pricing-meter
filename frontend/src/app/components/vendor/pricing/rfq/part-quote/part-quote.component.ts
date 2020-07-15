1e89b943384e9e7276e50d58728479de25985c03
sarvaratchagan <sarvaratchagan@outlook.com>
2020-07-15 23:24:09 +0200

    feat - part quote dummy component

---
+++ frontend/src/app/components/vendor/pricing/rfq/part-quote/part-quote.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-part-quote',
+  templateUrl: './part-quote.component.html',
+  styleUrls: ['./part-quote.component.css']
+})
+export class PartQuoteComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
