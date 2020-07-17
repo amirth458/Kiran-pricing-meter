82671a88290c0f677b699a5f07e68c415279da16
sarvaratchagan <sarvaratchagan@outlook.com>
2020-07-17 21:15:52 +0200

    fix - inject required services to super class component

--- frontend/src/app/components/vendor/projects/customer-accepted/customer-accepted.component.ts
+++ frontend/src/app/components/vendor/projects/customer-accepted/customer-accepted.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { NgxSpinnerService } from 'ngx-spinner';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { BiddingService } from '../../../../service/bidding.service';
+import { PartService } from '../../../../service/part.service';
 import { PmReleaseQueueComponent } from '../pm-release-queue/pm-release-queue.component';
 
 @Component({
@@ -12,8 +14,14 @@ import { PmReleaseQueueComponent } from '../pm-release-queue/pm-release-queue.co
   styleUrls: ['./../pm-release-queue/pm-release-queue.component.css', './customer-accepted.component.css']
 })
 export class CustomerAcceptedComponent extends PmReleaseQueueComponent implements OnInit {
-  constructor(public spinner: NgxSpinnerService, public router: Router, public biddingService: BiddingService) {
-    super(spinner, router, biddingService);
+  constructor(
+    public spinner: NgxSpinnerService,
+    public router: Router,
+    public biddingService: BiddingService,
+    public partService: PartService,
+    public modalService: NgbModal
+  ) {
+    super(spinner, router, biddingService, partService, modalService);
   }
 
   ngOnInit() {
