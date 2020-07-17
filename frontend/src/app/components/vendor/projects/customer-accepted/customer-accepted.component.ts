880b98486cb5f7ab342c2342d0b59da73829f054
sarvaratchagan <sarvaratchagan@outlook.com>
2020-07-17 19:57:42 +0200

    feat - customer accepted proposal component

---
+++ frontend/src/app/components/vendor/projects/customer-accepted/customer-accepted.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { NgxSpinnerService } from 'ngx-spinner';
+
+import { BiddingService } from '../../../../service/bidding.service';
+import { PmReleaseQueueComponent } from '../pm-release-queue/pm-release-queue.component';
+
+@Component({
+  selector: 'app-customer-accepted',
+  templateUrl: './../pm-release-queue/pm-release-queue.component.html',
+  styleUrls: ['../../pm-release-queue/pm-release-queue.component.css', './customer-accepted.component.css']
+})
+export class CustomerAcceptedComponent extends PmReleaseQueueComponent implements OnInit {
+  constructor(public spinner: NgxSpinnerService, public router: Router, public biddingService: BiddingService) {
+    super(spinner, router, biddingService);
+  }
+
+  ngOnInit() {
+    super.ngOnInit();
+  }
+}
