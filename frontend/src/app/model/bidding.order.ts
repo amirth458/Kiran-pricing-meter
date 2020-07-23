4235f4ce2313ded949a3c4f760d7de5025ce18dc
sarvaratchagan <sarvaratchagan@outlook.com>
2020-07-23 20:18:22 +0200

    feat - proposal part schema

--- frontend/src/app/model/bidding.order.ts
+++ frontend/src/app/model/bidding.order.ts
@@ -124,3 +124,83 @@ export interface MinimumProposalInfo {
   offerId: number;
   proposalPartIds: Array<number>;
 }
+
+export interface AdminProposalRequest {
+  part: ProposalPart;
+  partQuote: ProposalPartQuote;
+  partDimensionUpdated: boolean;
+  referenceMedias: Array<ProposalReferenceMedia>;
+}
+
+export interface ProposalReferenceMedia {
+  name: string;
+  uploadedAt: Date;
+  location: string;
+}
+
+interface DimensionUnit {
+  value: number;
+  unitId: number;
+}
+
+export interface ProposalPartDimension {
+  x: DimensionUnit;
+  y: DimensionUnit;
+  z: DimensionUnit;
+  volume: DimensionUnit;
+  surfaceArea: DimensionUnit;
+  thumbnail100Location: string;
+  thumbnail200Location: string;
+  thumbnail400Location: string;
+}
+
+export interface ProposalMedia {
+  connectorServiceId: number;
+  uploadedAt: Date;
+  location: string;
+  name: string;
+  partDimension: ProposalPartDimension;
+}
+
+export interface ProposalPartRfqMedia {
+  media: ProposalMedia;
+}
+
+export interface ProposalPart {
+  materialPropertyType: string;
+  materialPropertyValues: Array<string>;
+  equipmentPropertyType: string;
+  equipmentPropertyValues: Array<string>;
+  cuttingBondingAllowed: boolean;
+  quantity: number;
+  targetDeliveryDate: Date;
+  manualPricingAllowed: boolean;
+  parentPartId: number;
+  comments: string;
+  rfqMedia: ProposalPartRfqMedia;
+  postProcessTypeIds: Array<number>;
+}
+
+export interface ProposalQuoteDetails {
+  extendedCost: number;
+  invoiceCost: number;
+  invoiceItemId: number;
+  invoiceLineItemId: number;
+  partQuoteId: number;
+  processPricingConditionTypeId: number;
+  unit: number;
+  unitPrice: number;
+}
+
+export interface ProposalPartQuote {
+  isAdminQuote: boolean;
+  expiredAt: Date;
+  isManualPricing: boolean;
+  isGlobalRule: boolean;
+  isAutoQuoteOverride: boolean;
+  globalRuleReason: Array<number>;
+  partQuoteDetailList: Array<ProposalQuoteDetails>;
+  totalCost: number;
+  adminMargin: number;
+  vendorId: number;
+}
