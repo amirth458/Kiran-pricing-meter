7e891713f6b98dc90cacb08743f4471a51b8affd
sarvaratchagan <sarvaratchagan@outlook.com>
2020-07-28 15:44:37 +0200

    feat - add new vendor in suppliers queue

--- frontend/src/app/components/vendor/projects/add-supplier/add-supplier.component.ts
+++ frontend/src/app/components/vendor/projects/add-supplier/add-supplier.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, TemplateRef, ViewChild } from '@angular/core';
 
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ColDef, GridOptions } from 'ag-grid-community';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
 
 import { VendorConfirmationResponse } from '../../../../model/bidding.order';
 import { TemplateRendererComponent } from '../../../../common/template-renderer/template-renderer.component';
@@ -15,12 +16,23 @@ import { ProjectService } from '../../../../service/project.service';
 })
 export class AddSupplierComponent implements OnInit {
   @ViewChild('templateRef') templateRef: TemplateRef<any>;
-  @ViewChild('vendorCell') vendorCell: TemplateRef<any>;
+  @ViewChild('checkBoxCell') checkBoxCell: TemplateRef<any>;
 
+  bidProjectId: number;
+  @Input()
+  set value(value: number) {
+    this.bidProjectId = value;
+  }
+  get value(): number {
+    return this.bidProjectId;
+  }
   @Input()
   exclude: VendorConfirmationResponse[];
   @Input()
   partIds: Array<number>;
+
+  @Output()
+  refresh: EventEmitter<any> = new EventEmitter<any>();
 
   colDefs: ColDef[] = [];
   gridOptions: GridOptions;
@@ -30,10 +42,15 @@ export class AddSupplierComponent implements OnInit {
   availableSuppliers = [];
   selectableCount: number;
 
+  get isReleaseQueueAvailable(): boolean {
+    return (this.availableSuppliers || []).some(item => item.selected);
+  }
+
   constructor(
     public modelService: NgbModal,
     public projectService: ProjectService,
-    public spinner: NgxSpinnerService
+    public spinner: NgxSpinnerService,
+    public toaster: ToastrService
   ) {}
 
   ngOnInit() {
@@ -50,6 +67,24 @@ export class AddSupplierComponent implements OnInit {
 
   setupGrid() {
     this.colDefs = [
+      {
+        headerName: '',
+        field: 'selected',
+        hide: false,
+        sortable: false,
+        filter: false,
+        width: 60,
+        maxWidth: 60,
+        cellClass: 'p-0 check-box-column',
+        cellRenderer: 'templateRenderer',
+        cellRendererParams: {
+          ngTemplate: this.checkBoxCell
+        },
+        cellEditor: 'templateRenderer',
+        cellEditorParams: {
+          ngTemplate: this.checkBoxCell
+        }
+      },
       {
         headerName: 'No',
         valueGetter: 'node.rowIndex + 1',
@@ -83,6 +118,7 @@ export class AddSupplierComponent implements OnInit {
         const partCertificates = (item.partCertificates || []).map(partCert => partCert.name);
         return {
           ...item,
+          selected: false,
           vendorName: item.vendorName,
           vendorType: item.vendorType.name,
           country: item.country.name,
@@ -96,13 +132,50 @@ export class AddSupplierComponent implements OnInit {
       this.availableSuppliers = availableSuppliers.filter(
         v => this.exclude.find(s => s.vendorId === v.vendorId) === undefined
       );
-      console.log(this.availableSuppliers);
       this.modelService.open(this.templateRef, {
         centered: true,
         size: 'lg'
       });
       this.spinner.hide();
     });
+  }
+
+  release() {
+    if (this.isReleaseQueueAvailable) {
+      this.spinner.show();
+      const selectedProfiles = this.availableSuppliers
+        .filter(item => item.selected)
+        .map(item => ({
+          vendorId: item.vendorId,
+          releasePriority: item.releasePriority
+        }))
+        .sort((a, b) => a.releasePriority - b.releasePriority);
+      this.projectService
+        .saveReleasePMBidToVendor({
+          bidPmProjectId: this.value,
+          releaseBidPmProjectToVendors: selectedProfiles
+        })
+        .subscribe(
+          response => {
+            this.spinner.hide();
+            const obj: any = (response || []).length > 0 ? response[0] : null;
+            if ((response || []).length > 0) {
+              this.refresh.emit();
+              this.toaster.success('Successfully released!');
+            } else {
+              this.toaster.error('unable to release to vendor');
+            }
+            this.modelService.dismissAll();
+          },
+          error => {
+            this.modelService.dismissAll();
+            this.spinner.hide();
+            this.toaster.error('Error while releasing to Vendor.');
+          }
+        );
+    } else {
+      this.toaster.error('Please add vendors in release queue!');
+    }
   }
 
   onSuppliersGridReady(ev) {
