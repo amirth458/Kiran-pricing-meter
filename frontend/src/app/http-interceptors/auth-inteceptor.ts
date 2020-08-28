a760c124ebdef393444a8c687af8bde3e5f30761
Mike O <mikiyasolika@gmail.com>
2020-08-28 19:50:20 +0300

    feat: setting proper headers

--- frontend/src/app/http-interceptors/auth-inteceptor.ts
+++ frontend/src/app/http-interceptors/auth-inteceptor.ts
@@ -5,22 +5,22 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const localAuthInfo = localStorage.getItem('admin-auth'); // you probably want to store it in localStorage or something
+    const localAuthInfo = localStorage.getItem('admin-auth');
 
     if (!localAuthInfo || req.headers.get('Authorization')) {
       const req1 = req.clone({
         headers: req.headers
-        // .set('Content-Type', 'application/json; charset=utf-8')
-        // .set('X-Content-Type-Options', 'nosniff')
       });
       return next.handle(req1);
     }
 
     const token = JSON.parse(localAuthInfo);
     const req2 = req.clone({
-      headers: req.headers.set('Authorization', `${token.tokenType} ${token.accessToken}`)
-      // .set('Content-Type', 'application/json; charset=utf-8')
-      // .set('X-Content-Type-Options', 'nosniff')
+      headers: req.headers
+        .set('Authorization', `${token.tokenType} ${token.accessToken}`)
+        .set('X-Content-Type-Options', 'nosniff')
+        .set('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate')
+        .set('Pragma', 'no-cache')
     });
     return next.handle(req2);
   }
