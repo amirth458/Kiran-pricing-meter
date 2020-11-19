995481c3a34dd67892b75c45341d66d906da84da
amirth458 <amirtharaj@softsuave.com>
2020-11-20 01:27:27 +0530

    Home page

--- pricing-meter-frontEnd/src/app/Component/agent/login/login.component.ts
+++ pricing-meter-frontEnd/src/app/Component/agent/login/login.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Injector, OnInit} from '@angular/core';
+import {AppComponentBase} from "../../../Shared/AppBaseComponent";
+import {LoginDialogComponent} from "../login-dialog/login-dialog.component";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent extends AppComponentBase implements OnInit {
 
-  constructor() { }
+  constructor(inject: Injector) {
+    super(inject);
+  }
 
   ngOnInit(): void {
   }
+
+  navigateTo(): void {
+    this.router.navigate(['agent/register-step-one']);
+  }
+
+  loginPopUp(): void {
+    const dialogRef = this.dialog.open(LoginDialogComponent, {
+      data: {action: 'login'},
+      width: '550px',
+      height: '753px'
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+      }
+    });
+  }
+
 
 }
